f768c2e6f4cfc9f1af596e2bfb5a4bfb15de254e
srook12 <srook12@gmail.com>
2016-09-15 17:07:29 -0400

    Initial submission of Assignment 19

---
+++ Day24/src/ssa/Mainline.java
@@ -0,0 +1,12 @@
+package ssa;
+
+import java.sql.SQLException;
+
+public class Mainline {
+
+	public static void main(String[] args) throws SQLException {
+		EducationSystem es = new EducationSystem();
+
+	}
+
+}
